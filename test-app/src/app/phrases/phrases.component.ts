ccac936fe68fff79f0d8414752fab6ee3f48fb52
Dmytro <dima.mattock@gmail.com>
2020-11-28 11:10:32 +0200

    first test Angular App

---
+++ test-app/src/app/phrases/phrases.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {PHRASES} from '../core/data/input-phrases';
+
+@Component({
+  selector: 'app-phrases',
+  templateUrl: './phrases.component.html',
+  styleUrls: ['./phrases.component.scss']
+})
+export class PhrasesComponent implements OnInit {
+  phrasesList = PHRASES.slice();
+  currentValue: string;
+  currentLanguage: string;
+
+  constructor() {
+    this.currentValue = '';
+    this.currentLanguage = '';
+   }
+
+  ngOnInit(): void {}
+
+  addPhrase(): void {
+    if (this.currentValue && this.currentLanguage) {
+      this.phrasesList.push({value: this.currentValue, language: this.currentLanguage})
+    }
+  }
+
+}
