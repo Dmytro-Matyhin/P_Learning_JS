ef5007ebec8d034bfdaf3ff69676c271ab156565
Dmytro <dima.mattock@gmail.com>
2020-11-29 16:11:01 +0200

    change shadow-root styles

--- lesson 22/main.js
+++ lesson 22/main.js
@@ -22,6 +22,21 @@ export class AlertComponent extends HTMLElement {
         padding: 0 10px;
         border: 1px solid;
       }
+      :host-context([type="error"]) {
+        border-color: rgb(252, 128, 175); 
+        background-color: rgb(241, 178, 202); 
+        color: rgb(128, 60, 86);
+      }
+      :host-context([type="success"]) {
+        border-color: rgb(39, 221, 136); 
+        background-color: rgb(118, 233, 179); 
+        color: rgb(0, 92, 49);
+      }
+      :host-context([type="info"]) {
+        border-color: rgb(2, 179, 202);
+        background-color: rgb(150, 211, 219);
+        color: rgb(6, 54, 61);
+      }
     </style>
   `
   }
@@ -33,38 +48,8 @@ export class AlertComponent extends HTMLElement {
   }
 
   attributeChangedCallback(name, prev, curr) {
-    switch (name) {
-      case 'message': 
+    if (name == 'message') {
       this.message = curr;
-      break;
-      case 'type': {
-        switch (curr) {
-          case 'error': 
-          this.style =
-           `
-          border-color: rgb(252, 128, 175); 
-          background-color: rgb(241, 178, 202); 
-          color: rgb(128, 60, 86);
-          `
-          break;
-          case 'success': 
-          this.style = 
-          `
-          border-color: rgb(39, 221, 136); 
-          background-color: rgb(118, 233, 179); 
-          color: rgb(0, 92, 49);
-          `
-          break;
-          case 'info': 
-          this.style =
-          `
-          border-color: rgb(2, 179, 202);
-          background-color: rgb(150, 211, 219);
-          color: rgb(6, 54, 61);
-          `
-          break;
-        }
-      }
     }
   }
 }
